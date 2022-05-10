e1e28e76ac35400d87cd481560aa08d23644ff5e
Willian Oliveira <will.santos92@gmail.com>
2022-05-10 09:03:58 -0300

    Adicionando informações importantes

--- operador-curto.go
+++ operador-curto.go
@@ -5,10 +5,12 @@ import (
 )
 
 //Variaveis declaradas aqui, funcionam em todo o código
+//Para atribuir valores a variaveis existentes usamos o sinal =
+//O valor de atribuição, só é atribuido a variaveis existentes
 
 func main() { //Isso aqui é o codeblock.
 
-	// a marmota := só funciona dentro do codeblock
+	// a marmota := só funciona dentro do codeblock e é utilizada para criar novas variaveis
 
 	x := 10
 	y := "Olá bom dia"
