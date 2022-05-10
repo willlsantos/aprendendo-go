72fb97c83c3064f1ecbd81994fd7b17f55d22b33
Willian Oliveira <will.santos92@gmail.com>
2022-05-10 20:20:11 -0300

    entendendo os valores zero de variaveis não inicializadas

---
+++ valor-zero.go
@@ -0,0 +1,34 @@
+/* Usar := sempre que possivel (Declarador curto de operação)
+   Usar o var para criar variaveis package-level scope
+   Declaração vs Inicialização vs Atribuição de valor das variaveis. Ex: Caixas postais
+
+   Os zeros:
+	- ints: 0
+	- floats: 0.0
+	- booleans: false
+	- strings: ""
+	- pointes, functions, interfaces, slices, channels, maps: nil */
+
+package main
+
+import (
+	"fmt"
+)
+
+var x int
+var a int
+var b float64
+var c string
+var d bool
+
+func main() {
+	//x = 10 //Inicialização e atribuição de valor da variavel
+	//x = 20 //Apenas atribuição de valor da variavel
+	x := 10
+	//Abaixo print de variaveis declaradas porém não inicializadas.
+	fmt.Printf("%v, %T\n", a, a)
+	fmt.Printf("%v, %T\n", b, b)
+	fmt.Printf("%v, %T\n", c, c)
+	fmt.Printf("%v, %T\n", d, d)
+	fmt.Printf("%v, %T\n", x, x)
+}
